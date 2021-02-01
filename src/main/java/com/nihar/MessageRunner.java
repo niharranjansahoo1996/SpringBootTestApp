5bd2faa3ea9519451574fa1d22d836da1c243d25
TL <TL@gmail.com>
2021-02-01 05:35:47 -0500

    TS-485 Modification done by i value

--- src/main/java/com/nihar/MessageRunner.java
+++ src/main/java/com/nihar/MessageRunner.java
@@ -8,7 +8,7 @@ public class MessageRunner implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("WELCOME TO SPRINGBOOT APPLICATION BY GIT");
-		System.out.println("Some modifications are done");
+		int i=10;
 		System.out.println("Some modifications for second time");
 	}
 
